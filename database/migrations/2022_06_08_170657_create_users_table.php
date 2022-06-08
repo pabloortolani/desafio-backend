fe24fa282c2409bdb03ec28f1d913f7970d50f59
Pablo Ortolani <pabloortolani@gmail.com>
2022-06-08 14:15:01 -0300

    Criação das migrations das tabelas user_types, users e wallets.

--- database/migrations/2022_06_08_170657_create_users_table.php
+++ database/migrations/2022_06_08_170657_create_users_table.php
@@ -16,11 +16,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('document', 14)->unique();
+            $table->unsignedBigInteger('type_id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('type_id', 'fk_type_user')
+                ->references('id')->on('user_types');
         });
     }
 
--- database/migrations/2022_06_08_171023_create_wallets_table.php
+++ database/migrations/2022_06_08_171023_create_wallets_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->float('balance')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id', 'fk_user_id')
+                ->references('id')->on('users');
         });
     }
 
@@ -27,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('wallets');
     }
 };
